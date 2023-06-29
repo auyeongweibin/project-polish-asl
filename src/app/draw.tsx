63a03ab74bae8b5cfb6e383768d965250b615c24
Wei Bin <auyeongweibin@gmail.com>
2023-06-30 04:11:09 +0800

    added nms functions

--- src/app/draw.tsx
+++ src/app/draw.tsx
@@ -3,28 +3,145 @@ import * as tf from "@tensorflow/tfjs";
 // Define our labels
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
 
-export const drawRect = async (boxes: Array<Array<number>>, scores: Array<Array<number>>, threshold: number, imgWidth: number, imgHeight: number, ctx: any)=>{
+const modelInput:number = 384;
+const imageWidth:number = 1280;
+const imageHeight:number = 720
 
-  const maxScores = scores.map(row => Math.max(...row))
+const preprocess = (source: any, modelWidth: number, modelHeight: number) => {
+  const img = tf.browser.fromPixels(source);
+  const [h, w] = img.shape.slice(0, 2); // get source width and height
+  const maxSize = Math.max(w, h); // get max size
+  const imgPadded:tf.Tensor3D = img.pad([
+    [0, maxSize - h], // padding y [bottom only]
+    [0, maxSize - w], // padding x [right only]
+    [0, 0],
+  ]);
 
-  const result = await tf.image.nonMaxSuppressionWithScoreAsync(boxes, maxScores, 1, 0.45, threshold);
+  const resized = tf.image.resizeBilinear(imgPadded, [modelWidth,modelHeight]).expandDims(0);
 
-  let index: number = result['selectedIndices'].dataSync()[0];;
-  let score: number = result['selectedScores'].dataSync()[0];
-  let character : string = alphabet[scores[index].indexOf(Math.max(...scores[index]))];
+  return resized
+}
 
-  // Extract variables
-  const [x,y,height,width] = boxes[index];
+const postprocess = (output: any, maxBoxes: number, iouThreshold: number, probThreshold: number) => {
+  const data = tf.tidy(() => output.transpose([0, 2, 1]).squeeze()).arraySync(); // transpose main result
+  const numPreds = data.length;
+  let boxes:Array<Array<number>> = [];
+  for (let i=0;i<numPreds;i++) {
+    const scores = data[i].slice(4)
+    const prob = Math.max.apply(null, scores);
+    if (prob < probThreshold) {
+      continue;
+    }
+    const label = scores.indexOf(prob);
+    const xc = data[i][0];
+    const yc = data[i][1];
+    const w = data[i][2];
+    const h = data[i][3];
+    // const x1 = (xc-w/2)/modelInput*imageWidth;
+    // const y1 = (yc-h/2)/modelInput*imageHeight;
+    // const x2 = (xc+w/2)/modelInput*imageWidth;
+    // const y2 = (yc+h/2)/modelInput*imageHeight;
+    const x1 = (xc)/modelInput*imageWidth;
+    const y1 = (yc)/modelInput*imageHeight;
+    const x2 = (xc+w)/modelInput*imageWidth;
+    const y2 = (yc+h)/modelInput*imageHeight;
+    boxes.push([x1,y1,x2,y2,prob,label]);
+  }
+  boxes = boxes.sort((box1,box2) => box2[5]-box1[5]);
+  const result = [];
+  while (boxes.length>0) {
+      result.push(boxes[0]);
+      boxes = boxes.filter(box => iou(boxes[0],box)<iouThreshold);
+  }
 
-  // Set styling
-  ctx.strokeStyle = 'green'
-  ctx.lineWidth = 2
-  ctx.fillStyle = 'white'
-  ctx.font = '30px Arial'   
+  if (result.length > maxBoxes) {
+    return result.slice(0, maxBoxes + 1);
+  }
+
+  return result;
+}
+
+const union = (box1:Array<number>, box2:Array<number>) =>  {
+  const [box1_x1,box1_y1,box1_x2,box1_y2] = box1;
+  const [box2_x1,box2_y1,box2_x2,box2_y2] = box2;
+  const box1_area = (box1_x2-box1_x1)*(box1_y2-box1_y1)
+  const box2_area = (box2_x2-box2_x1)*(box2_y2-box2_y1)
+  return box1_area + box2_area - intersection(box1,box2)
+}
+
+const intersection = (box1:Array<number>, box2:Array<number>) => {
+  const [box1_x1,box1_y1,box1_x2,box1_y2] = box1;
+  const [box2_x1,box2_y1,box2_x2,box2_y2] = box2;
+  const x1 = Math.max(box1_x1,box2_x1);
+  const y1 = Math.max(box1_y1,box2_y1);
+  const x2 = Math.min(box1_x2,box2_x2);
+  const y2 = Math.min(box1_y2,box2_y2);
+  return (x2-x1)*(y2-y1)
+}
+
+const iou = (box1:Array<number>, box2:Array<number>) => {
+  return intersection(box1,box2)/union(box1,box2);
+}
+
+export const detectVideo = (videoSource: any, model: any, canvasRef: any) => {
+  const detect = async () => {
+    if (videoSource.srcObject === null) {
+      const ctx = canvasRef.getContext("2d");
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+      return; 
+    } // if webcam is closed
+
+    tf.engine().startScope(); // start scoping tf engine
+
+    const [modelHeight, modelWidth] = model.inputs[0].shape.slice(1, 3);
+
+    const resized = preprocess(videoSource, modelWidth, modelHeight);
+
+    const res = model.execute(resized); // execute model
+    const output = postprocess(res, 1, 0.7, 0.9);
+ 
+    const ctx = canvasRef.getContext("2d");
+
+    // Set styling
+    ctx.strokeStyle = 'green'
+    ctx.lineWidth = 1
+    ctx.fillStyle = 'white'
+    ctx.font = '14px Arial'   
+      
+    output.slice(0, 5).forEach((r:Array<number>) => {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+
+      const [x1, y1, x2, y2, _, label] = r
+      const y = Math.floor(y1)
+      const x = Math.floor(x1)
+      const h = Math.round(y2-y1) 
+      const w = Math.round(x2-x1)
   
-  // DRAW!!
-  ctx.beginPath()
-  ctx.fillText(character + "-" + score, x*imgWidth/384-width*imgWidth/384, y*imgHeight/384-height*imgHeight/384-10)
-  ctx.rect(x*imgWidth/384-width*imgWidth/384, y*imgHeight/384-height*imgHeight/384, width*imgWidth/192, height*imgHeight/192);
-  ctx.stroke()
+      // DRAW!!
+      ctx.beginPath()
+      ctx.fillText(alphabet[label], x, y-10);
+      ctx.rect(x, y, w, h);
+      ctx.stroke()
+    });
+
+    // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+
+    // const [y1, x1, y2, x2, _, label] = output[0]
+    // const y = Math.floor(y1)
+    // const x = Math.floor(x1)
+    // const h = Math.round(y2) 
+    // const w = Math.round(x2)
+
+    // // DRAW!!
+    // ctx.beginPath()
+    // ctx.fillText(alphabet[label], x, y-10);
+    // ctx.rect(x, y, w-x, h-y);
+    // ctx.stroke()
+
+    requestAnimationFrame(detect);
+  
+    tf.engine().endScope(); // end of scoping
+  }
+
+  detect();
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,61 +3,31 @@
 import Image from 'next/image';
 import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
-import Webcam from "react-webcam";
-import {drawRect} from "./draw"; 
+import "@tensorflow/tfjs-backend-webgl"; 
+import {detectVideo} from "./draw"; 
 
 export default function Home() {
-  const webcamRef = useRef(null);
+  const cameraRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  let model: any = null;
 
   const initialise = async () => {
 
-    const model = await tf.loadGraphModel('https://objectstorage.ap-singapore-1.oraclecloud.com/n/ax7maqnmi2u7/b/project-polish-asl/o/model.json');
+    navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "environment",
+          },
+        })
+        .then((stream) => {
+          cameraRef.current!.srcObject = stream;
+        });
+    
+    cameraRef.current!.style.display = "block"
 
-    setInterval(() => {
-      detect(model);
-    }, 500.0);
-  };
-
-  const detect = async (model: any) => {
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current['video']['readyState'] === 4 &&
-      canvasRef.current !== null
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current['video'];
-      const videoWidth = webcamRef.current['video']['videoWidth'];
-      const videoHeight = webcamRef.current['video']['videoHeight'];
-
-      // Set video width
-      webcamRef.current['video']['width'] = videoWidth;
-      webcamRef.current['video']['height'] = videoHeight;
-
-      // Set canvas height and width
-      canvasRef.current!['width'] = videoWidth;
-      canvasRef.current!['height'] = videoHeight;
-
-      const img = tf.browser.fromPixels(video);
-      const resized = tf.image.resizeBilinear(img, [384,384]).expandDims(0);
-      const obj = await model.execute(resized);
-
-      const result = await obj.array();
-
-      const boxes = result[0].slice(0, 4)[0].map((_: any, colIndex: any) => result[0].slice(0, 4).map((row: Array<number>) => row[colIndex]));
-      const scores = result[0].slice(4)[0].map((_: any, colIndex: any) => result[0].slice(4).map((row: Array<number>) => row[colIndex]));
-      
-      const ctx = canvasRef.current.getContext('2d');
-
-      requestAnimationFrame(()=>{drawRect(boxes, scores, 0.8, videoWidth, videoHeight, ctx)}); 
-
-      tf.dispose(img);
-      tf.dispose(resized);
-      tf.dispose(obj);
-      tf.dispose(result);
-
-    }
+    model = await tf.loadGraphModel('https://objectstorage.ap-singapore-1.oraclecloud.com/n/ax7maqnmi2u7/b/project-polish-asl/o/model.json');
   };
 
   useEffect(()=>{initialise()},[]);
@@ -84,9 +54,10 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <Webcam
-        ref={webcamRef}
-        muted={true} 
+      <video
+        autoPlay
+        muted
+        ref={cameraRef}
         style={{
           position: "absolute",
           marginLeft: "auto",
@@ -95,9 +66,10 @@ export default function Home() {
           right: 0,
           textAlign: "center",
           zIndex: 9,
-          width: 640,
-          height: 480,
+          width: 1280,
+          height: 720,
         }}
+        onPlay={() => detectVideo(cameraRef.current, model, canvasRef.current)}
       />
       <canvas
         ref={canvasRef}
@@ -109,8 +81,8 @@ export default function Home() {
           right: 0,
           textAlign: "center",
           zIndex: 10,
-          width: 640,
-          height: 480,
+          width: 1280,
+          height: 720,
         }}
       />
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 before:lg:h-[360px]">
